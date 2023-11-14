7d91eff8f4407233659097dce5bd5c87a3905e2e
Chrysthofer <chrys.mensagem@gmail.com>
2023-11-14 16:44:33 -0300

    Initial Commit

---
+++ Source/teste.asm
@@ -0,0 +1,17 @@
+#PRNAME atualizacao
+#DIRNAM "C:\Users\Lucca\Documents\Iniciacao\atualizacao\Hardware\atualizacao_H"
+#DATYPE 1
+#NBMANT 19
+#NBEXPO 8
+#NDSTAC 4
+#SDEPTH 4
+#NUIOIN 4
+#NUIOOU 4
+@main LOAD 2
+SET maina
+LOAD 3
+SET mainb
+LOAD mainb
+ADD maina
+SET mainc
+@fim JMP fim
---
+++ Source/teste.c
@@ -0,0 +1,20 @@
+#PRNAME atualizacao
+#DIRNAM "C:\Users\Lucca\Documents\Iniciacao\atualizacao\Hardware\atualizacao_H"
+#DATYPE 1
+#NBMANT 19
+#NBEXPO 8
+#NDSTAC 4
+#SDEPTH 4
+#NUIOIN 4
+#NUIOOU 4
+
+
+
+    void main()
+    {
+
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        
+    }
